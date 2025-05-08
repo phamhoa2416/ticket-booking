120f8bf0e51ec4d18e3bb0c31fb70ff805ea8c7f
phamviethoa <phamviethoa2416@gmail.com>
2025-05-09 00:34:54 +0700

    feat: add user module for di

---
+++ src/main/kotlin/users/di/UserModule.kt
@@ -0,0 +1,13 @@
+package users.di
+
+import org.koin.dsl.module
+import users.controllers.UserController
+import users.repository.UserRepository
+import users.repository.UserRepositoryImpl
+import users.service.UserService
+
+val userModule = module {
+    single<UserRepository> { UserRepositoryImpl() }
+    single { UserService(get()) }
+    single { UserController() }
+}
