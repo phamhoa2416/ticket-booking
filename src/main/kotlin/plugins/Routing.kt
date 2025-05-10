4f32098863ff0a56bb39b66c72dc2144aa37ecfa
phamviethoa <phamviethoa2416@gmail.com>
2025-05-10 19:16:11 +0700

    feat: import userController

--- src/main/kotlin/plugins/Routing.kt
+++ src/main/kotlin/plugins/Routing.kt
@@ -1,11 +1,13 @@
-package com.example.plugins
+package plugins
 
 import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.http.content.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.server.routing.*
+import org.koin.ktor.ext.inject
+import users.controllers.UserController
 
 fun Application.configureRouting() {
     install(StatusPages) {
@@ -14,10 +16,12 @@ fun Application.configureRouting() {
         }
     }
     routing {
+        val userController: UserController by inject()
+        with(userController) {
+            userRoutes()
+        }
         get("/") {
             call.respondText("Hello World!")
         }
-        // Static plugin. Try to access `/static/index.html`
-        staticResources("/static", "static")
     }
 }
