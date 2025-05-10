69fbe158b330347c542e63487affb83658b485aa
phamviethoa <phamviethoa2416@gmail.com>
2025-05-10 19:15:58 +0700

    feat: install ContentNegotiation plugin

--- src/main/kotlin/plugins/HTTP.kt
+++ src/main/kotlin/plugins/HTTP.kt
@@ -1,17 +1,15 @@
-package com.example.plugins
+package plugins
 
-import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
-import io.ktor.server.plugins.cors.routing.*
+import io.ktor.server.plugins.contentnegotiation.*
+import kotlinx.serialization.json.Json
 
 fun Application.configureHTTP() {
-    install(CORS) {
-        allowMethod(HttpMethod.Options)
-        allowMethod(HttpMethod.Put)
-        allowMethod(HttpMethod.Delete)
-        allowMethod(HttpMethod.Patch)
-        allowHeader(HttpHeaders.Authorization)
-        allowHeader("MyCustomHeader")
-        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+    install(ContentNegotiation) {
+        json(Json {
+            prettyPrint = true
+            isLenient = true
+        })
     }
 }
