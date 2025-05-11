73304ea0fc06b4e00fe91b93cd08d47d8142be88
phamviethoa <phamviethoa2416@gmail.com>
2025-05-11 10:33:53 +0700

    fix: routing the user controller

--- src/main/kotlin/plugins/Routing.kt
+++ src/main/kotlin/plugins/Routing.kt
@@ -3,11 +3,13 @@ package plugins
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.routing.*
 import org.koin.ktor.ext.inject
 import users.controllers.UserController
+import users.di.UserDependencies
 
 fun Application.configureRouting() {
     install(StatusPages) {
@@ -16,12 +18,9 @@ fun Application.configureRouting() {
         }
     }
     routing {
-        val userController: UserController by inject()
+        val userController: UserController = UserDependencies.userController
         with(userController) {
             userRoutes()
-        }
-        get("/") {
-            call.respondText("Hello World!")
         }
     }
 }
