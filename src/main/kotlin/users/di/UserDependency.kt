8a0cda70fb3c3b86d7e6d365853c201e186a5bf7
phamviethoa <phamviethoa2416@gmail.com>
2025-05-12 08:12:32 +0700

    fix: add customer dependency

--- src/main/kotlin/users/di/UserDependency.kt
+++ src/main/kotlin/users/di/UserDependency.kt
@@ -1,12 +1,22 @@
 package users.di
 
+import users.controllers.CustomerController
 import users.controllers.UserController
+import users.repository.CustomerRepository
+import users.repository.CustomerRepositoryImpl
 import users.repository.UserRepository
 import users.repository.UserRepositoryImpl
+import users.service.CustomerService
 import users.service.UserService
 
 object UserDependencies {
     private val userRepository: UserRepository = UserRepositoryImpl()
-    private val userService: UserService = UserService(userRepository)
+    private val customerRepository: CustomerRepository = CustomerRepositoryImpl()
+
+
+    private val userService: UserService = UserService(userRepository, customerRepository)
     val userController: UserController = UserController(userService)
+
+    private val customerService: CustomerService = CustomerService(customerRepository, userRepository)
+    val customerController: CustomerController = CustomerController(customerService)
 }
